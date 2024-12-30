a4350d43648d902182dcfa1b0c5f4695b770638b
Martinamanteiga <martinaamanteiga@gmail.com>
2024-12-30 21:12:08 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
   // Función para cambiar la imagen
   function changeImage(sport) {
     const images = {
-      soccer: 'https://www.bubblefootball.es/wp-content/uploads/2016/07/por-que-se-juega-11-jugadores-equipo-futbol.jpg',
-      basketball: 'https://jimsports.com/wp-content/uploads/2023/04/baloncesto-historia-curiosidades.jpg',
-      tennis: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Tennis_Racket_and_Balls.jpg/800px-Tennis_Racket_and_Balls.jpg',
-      baseball: 'https://static.vecteezy.com/system/resources/previews/028/668/229/non_2x/baseball-ball-generate-ai-photo.jpg'
+      soccer: 'images/futbol2.jpg',
+      basketball: 'images/baloncesto2.jpg',
+      tennis: 'images/tenis2.jpg',
+      baseball: 'images/beisbol2.jpg'
     };
 
     const imageElement = document.getElementById(sport).querySelector('img');
